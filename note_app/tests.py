1f283b8251afd9ce64280f5101a5d4bff1d74a04
20242174-RALPH\admin <oloresralph@gmail.com>
2025-03-20 10:32:02 +1300

    test all branch

--- note_app/tests.py
+++ note_app/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+# tests/test_api.py
+from django.test import TestCase, Client
+from django.urls import reverse
+from rest_framework import status
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient
 
-# Create your tests here.
+class LoginAPITestCase(TestCase):
+    def setUp(self):
+        x = 1
+        y = 1
+        self.assertEqual(x+y, 2)
+
+    def testcal(self):
+        self.assertEqual(1+1, 2);

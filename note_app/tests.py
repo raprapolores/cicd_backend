acc41475e6c3131feec8e7eca2d0998229faf899
20242174-RALPH\admin <oloresralph@gmail.com>
2025-03-24 15:15:59 +1300

    modified yaml and test cases files

--- note_app/tests.py
+++ note_app/tests.py
@@ -1,15 +1,30 @@
-# tests/test_api.py
-from django.test import TestCase, Client
+# test_auth.py
 from django.urls import reverse
+from rest_framework.test import APITestCase
 from rest_framework import status
 from django.contrib.auth.models import User
-from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
 
-class LoginAPITestCase(TestCase):
+
+class UserLoginTestCase(APITestCase):
     def setUp(self):
-        x = 1
-        y = 1
-        self.assertEqual(x+y, 2)
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret'
+        }
+        self.user = User.objects.create_user(**self.credentials)
+        self.token_url = reverse('login')
 
-    def testcal(self):
-        self.assertEqual(1+1, 2);
+    def test_token_login(self):
+        # Simulate login to get token
+        response = self.client.post(self.token_url, self.credentials)
+
+        # Check if the response status is 200 (OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Check if the token is in the response
+        self.assertIn('token', response.data)
+
+        # Use the token to authenticate further requests
+        token = response.data['token']
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
---
+++ test_utils.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+from note_app.utils import subtract, add
+
+
+class UtilsTestCase(TestCase):
+
+    def test_add(self):
+        self.assertEqual(add( 1, 2), 3)
+        self.assertEqual(add( -1, 2), 0)
+        self.assertEqual(add( 0, 0), 0)
+
+    def test_subtract(self):
+        self.assertEqual(subtract( 2, 1), 1)
+        self.assertEqual(subtract( 1, 1), 0)
+        self.assertEqual(subtract( 0, 1), -1)
